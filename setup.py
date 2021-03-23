dca717c103487c35b8f0dce9022bc20f20851179
Bjarne Riis <bjarne@briis.com>
2021-03-23 19:34:39 +0100

    bumped to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="pysecspy",
     packages=["pysecspy"],
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     description="Python Wrapper for SecuritySpy API",
     author="Bjarne Riis",
